06d263fe9d3687f2e3bede57d87e5f49c7519019
Lonami Exo <totufals@hotmail.com>
2017-11-14 10:18:14 +0100

    Dump retrieved messages to the database

--- main.py
+++ main.py
@@ -1,44 +1,63 @@
 #!/bin/env python3
-import os
 import json
-
-from telethon import TelegramClient
-from telethon.extensions import BinaryReader
-from telethon.errors import SessionPasswordNeededError, UsernameNotOccupiedError
-from json.decoder import JSONDecodeError
+import os
 from getpass import getpass
+from json.decoder import JSONDecodeError
 from time import sleep
 
-from telethon.tl.functions.messages import GetHistoryRequest
+from telethon import TelegramClient
+from telethon.errors import SessionPasswordNeededError
+from telethon.extensions import BinaryReader
+from telethon.tl import types as tl, functions as rpc
+from telethon.utils import get_peer_id
+
+from dumper import Dumper
 
 
-def save_messages(client, target):
-    params = {
-        'peer': target,
-        'offset_id': 0,
-        'offset_date': None,
-        'add_offset': 0,
-        'limit': 100,
-        'max_id': 0,
-        'min_id': 0
-    }
+def save_messages(client, dumper, target):
+    request = rpc.messages.GetHistoryRequest(
+        peer=target,
+        offset_id=0,
+        offset_date=None,
+        add_offset=0,
+        limit=100,
+        max_id=0,
+        min_id=0
+    )
+
     found = 0
-    total_messages = 0
     while True:
-        # TODO Actually save the messages, and the users
+        # TODO Actually save the the users
         # TODO Allow resuming
-        # TODO How shold edits be handled? Always read first two days?
-        history = client(GetHistoryRequest(**params))
+        # TODO How should edits be handled? Always read first two days?
+        history = client(request)
         if not history.messages:
             break
+
+        for m in history.messages:
+            fwd_id = None
+            if isinstance(m, tl.Message):
+                m.to_id = get_peer_id(m.to_id, add_mark=True)
+                if m.fwd_from:
+                    fwd_id = dumper.dump_forward(m.fwd_from)
+            elif isinstance(m, tl.MessageService):
+                m.to_id = get_peer_id(m.to_id, add_mark=True)
+                continue  # TODO Don't skip messageService's
+            else:
+                print('Skipping message', type(m).__name__)
+                continue
+
+            # TODO Handle media
+            dumper.dump_message(m, forward_id=fwd_id, media_id=None)
+
         found += len(history.messages)
         total_messages = getattr(history, 'count', len(history.messages))
-        params['offset_id'] = min(m.id for m in history.messages)
-        params['offset_date'] = min(m.date for m in history.messages)
+        request.offset_id = min(m.id for m in history.messages)
+        request.offset_date = min(m.date for m in history.messages)
         print('Downloaded {}/{} ({:.1%})'.format(
             found, total_messages, found / total_messages
         ))
-        sleep(0.5)  # TODO Smarter sleep, except FloodWait
+        sleep(1)
     print('Done.')
 
 
@@ -62,6 +81,8 @@ def fetch_dialogs(client, cache_file='dialogs.tl', force=False):
 
 
 if __name__ == '__main__':
+    dumper = Dumper(None)  # TODO Use actual config
+
     try:
         with open('client.conf') as f:
             conf = json.load(f)
@@ -79,7 +100,7 @@ if __name__ == '__main__':
                 client.sign_in(password=getpass())
 
         for entity in fetch_dialogs(client):
-            save_messages(client, entity)
+            save_messages(client, dumper, entity)
     except KeyboardInterrupt:
         pass
     finally:
