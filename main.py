e945e42305414169b6eae91e2cc1f989843dc08c
Tanuj <expectocode@users.noreply.github.com>
2017-11-15 18:42:35 +0000

    add DB name config option

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ def fetch_dialogs(client, cache_file='dialogs.tl', force=False):
 
 def load_config():
     # Load from file
-    defaults = {'ForceNoChangeDumpAfter':7200}
+    defaults = {'ForceNoChangeDumpAfter':7200,'DBFileName':'export'}
     config = configparser.ConfigParser(defaults)
     config.read('config.ini')
 
