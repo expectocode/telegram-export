de8c591e41edfe98428686996520a172963f819a
Tanuj <expectocode@users.noreply.github.com>
2018-02-25 16:33:40 +0000

    Allow datetimes and non-Bot API style IDs to be passed to formatter

--- base_formatter.py
+++ base_formatter.py
@@ -2,6 +2,10 @@
 """Utility to extract data from a telegram-export database"""
 from collections import namedtuple
 import sqlite3
+import datetime
+
+from telethon.utils import get_peer_id
+from telethon.tl import types
 
 
 Message = namedtuple('Message', (
@@ -45,10 +49,20 @@ class BaseFormatter:
         elif isinstance(db, sqlite3.Connection):
             self.dbconn = db
         else:
-            raise TypeError('Invalid database object given: %s', type(db))
+            raise TypeError('Invalid database object given: {}'.format(type(db)))
 
         self.our_userid = self.dbconn.execute(
             "SELECT UserID FROM SelfInformation").fetchone()[0]
+
+    @staticmethod
+    def get_timestamp(date):
+        if date is None or isinstance(date, int):
+            return date
+        if isinstance(date, datetime.datetime):
+            return date.timestamp()
+        if isinstance(date, datetime.date):
+            # Midnight at the start of that day
+            return datetime.datetime.combine(date, datetime.time()).timestamp()
 
     @staticmethod
     def _build_query(*args):
@@ -97,10 +111,14 @@ class BaseFormatter:
     def get_messages_from_context(self, context_id, start_date=None, end_date=None,
                                   from_user_id=None, order='DESC'):
         """
-        Yield Messages from a context. Start and end date should be UTC timestamps.
-        Note that Channels will never yield any messages if from_user_id is set,
-        as there is no FromID for Channel messages. Order is ASC or DESC.
+        Yield Messages from a context. Start and end date should be UTC timestamps
+        or datetime objects. Note that Channels will never yield any messages if
+        from_user_id is set, as there is no FromID for Channel messages. Order
+        should be ASC or DESC. Note that unlike the other methods, context_id
+        *must* be in the Bot API format where Channel/Supergroup IDs start with
+        -100 and old-style Chat IDs start with -.
         """
+        start_date, end_date = self.get_timestamp(start_date), self.get_timestamp(end_date)
         where, params = self._build_query(
             ('ContextID = ?', context_id),
             ('Date > ?', start_date),
@@ -117,21 +135,24 @@ class BaseFormatter:
         )
         row = cur.fetchone()
         if not row:
-            raise StopIteration
+            return
         out = self.our_userid == row[3]
         while row:
             yield Message(*row, out)
             row = cur.fetchone()
             if not row:
-                raise StopIteration
+                return
             out = self.our_userid == row[3]
 
     def get_user(self, uid, at_date=None):
         """
         Return the user with given ID or raise ValueError. If at_date is set,
         get the user as they were at the given date (to the best of our knowledge).
-        If it is not set, get the user as we last saw them.
+        If it is not set, get the user as we last saw them. at_date should be a UTC
+        timestamp or datetime object.
         """
+        uid = get_peer_id(types.PeerUser(uid))
+        at_date = self.get_timestamp(at_date)
         cur = self.dbconn.cursor()
         query = (
             "SELECT ID, DateUpdated, FirstName, LastName, Username, "
@@ -145,8 +166,11 @@ class BaseFormatter:
     def get_channel(self, cid, at_date=None):
         """
         Return the channel with given ID or raise ValueError. If at_date is set,
-        get the channel as it was at the given date (to the best of our knowledge)
+        get the channel as it was at the given date (to the best of our knowledge).
+        at_date should be a UTC timestamp or datetime object.
         """
+        at_date = self.get_timestamp(at_date)
+        uid = get_peer_id(types.PeerChannel(cid))
         cur = self.dbconn.cursor()
         query = (
             "SELECT ID, DateUpdated, About, Title, Username, "
@@ -160,8 +184,11 @@ class BaseFormatter:
     def get_supergroup(self, sid, at_date=None):
         """
         Return the supergroup with given ID or raise ValueError. If at_date is set,
-        get the supergroup as it was at the given date (to the best of our knowledge)
+        get the supergroup as it was at the given date (to the best of our knowledge).
+        at_date should be a UTC timestamp or datetime object.
         """
+        at_date = self.get_timestamp(at_date)
+        uid = get_peer_id(types.PeerChannel(sid))
         cur = self.dbconn.cursor()
         query = (
             "SELECT ID, DateUpdated, About, Title, Username, "
@@ -175,8 +202,11 @@ class BaseFormatter:
     def get_chat(self, cid, at_date=None):
         """
         Return the chat with given ID or raise ValueError. If at_date is set,
-        get the chat as it was at the given date (to the best of our knowledge)
+        get the chat as it was at the given date (to the best of our knowledge).
+        at_date should be a UTC timestamp or datetime object.
         """
+        at_date = self.get_timestamp(at_date)
+        uid = get_peer_id(types.PeerChat(cid))
         cur = self.dbconn.cursor()
         query = (
             "SELECT ID, DateUpdated, Title, MigratedToID, PictureID FROM Chat"
@@ -186,14 +216,14 @@ class BaseFormatter:
             raise ValueError("No chat with ID {} in database".format(cid))
         return Chat(*row)
 
-    def get_media(self, id):
+    def get_media(self, mid):
         """Return the Media with given ID or raise ValueError."""
         cur = self.dbconn.cursor()
         cur.execute("SELECT ID, Name, MimeType, Size, ThumbnailID, Type, LocalID, "
-                    "VolumeID, Secret, Extra FROM Media WHERE ID = ?", (id,))
+                    "VolumeID, Secret, Extra FROM Media WHERE ID = ?", (mid,))
         row = cur.fetchone()
         if not row:
-            raise ValueError("No media with ID {} in database".format(id))
+            raise ValueError("No media with ID {} in database".format(mid))
         return Media(*row)
 
 # if __name__ == '__main__':
